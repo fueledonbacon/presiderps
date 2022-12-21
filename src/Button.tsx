6a7b16b6fdad8f8e201bdb00de52e192952ce616
Byldr <ryan@fueledonbacon.com>
2022-12-21 15:20:52 -0500

    update

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from "react"
 export const Button = ({ disabled, onClick, children = "" }: { disabled?: boolean, onClick?: any, children: string | ReactElement | Array<ReactElement> }) => {
-    return (<button disabled={disabled} onClick={onClick} className="p-4 min-ws-md bg-yellow-400 rounded-lg font-bold cursor-pointer hover:bg-yellow-200" type="button">
+    return (<button disabled={disabled} onClick={onClick} className="p-4 max-w-sm w-full bg-yellow-500 text-white font-bold cursor-pointer hover:bg-yellow-200" type="button">
         {children}
     </button>)
 }
